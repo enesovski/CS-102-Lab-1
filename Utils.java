25867d53661296dfe4e33fc639e4a4472c7eb4fb
Enes Ceran <cceran52@gmail.com>
2024-09-25 15:53:19 +0300

    Enes commit

--- Utils.java
+++ Utils.java
@@ -1,2 +1,28 @@
 public class Utils{
+
+    //Enes
+    public static int SumOddIndexElements(int[] array)
+    {
+        int sum = 0;
+
+        for (int i = 1 ;i < array.length; i+=2) {
+            
+            sum += array[i];
+        }
+
+        return sum;
+    } 
+
+    public static int SumEvenIndexElements(int[] array)
+    {
+        int sum = 0;
+
+        for (int i = 0 ;i < array.length; i+=2) {
+            
+            sum += array[i];
+        }
+
+        return sum;
+    } 
+
 }
