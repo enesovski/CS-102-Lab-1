3304a80efca21505cc390eb64b17439b637ab6fc
Yavuz Selim Kahyaoğlu <yavuzskhy@gmail.com>
2024-09-25 15:58:07 +0300

    yavuz commit

--- Utils.java
+++ Utils.java
@@ -23,6 +23,28 @@ public class Utils{
         }
 
         return sum;
-    } 
+    }
+    
+    //yavuz
+    public static double[] AverageDifference(int[] array) {
+        
+        double sum = 0;
+        double avg;
+        double[] diffArray = new double[array.length];  
+        
+
+        for (double i : array) {
+            sum += i;
+        }
+
+        avg = sum / array.length;
+
+        for (int i = 0; i < diffArray.length; i++) {
+            diffArray[i] = array[i] - avg;
+        }
+        
+
+        return diffArray;
+    }
 
 }
